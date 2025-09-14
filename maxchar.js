40efa97a3af810db006de704b14a647ece84d410
Kenan Bharatbhai Gain <123521687+KenanGain@users.noreply.github.com>
2025-09-13 23:19:34 -0400

    maxchar.js added

---
+++ maxchar.js
@@ -0,0 +1,30 @@
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+
+    for ( let char of str ) {
+       if(charMap[char]) {
+              charMap[char] = charMap[char] + 1;
+         } else {
+            charMap[char] = 1;
+         }
+    }
+    // for ( const [key, value] of Object.entries(charMap)) {
+    //     if(value > max ) {
+    //         max = value;
+    //         maxChar = key;
+    //     }
+    // }
+
+    for ( let key in charMap) { 
+        if(charMap[key] > max ) {
+            max = charMap[key];
+            maxChar = key;
+        }
+    
+   
+}
+ return maxChar;
+}
+
+console.log(maxChar('Hello')); // Output: { h: 1, e: 1, l: 2, o: 1 }
