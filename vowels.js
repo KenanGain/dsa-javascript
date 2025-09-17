67acf525cdd7e7abb07847a448d73b14c4c76aab
Kenan Bharatbhai Gain <123521687+KenanGain@users.noreply.github.com>
2025-09-17 15:53:22 -0400

    Vowels

---
+++ vowels.js
@@ -0,0 +1,13 @@
+function vowels(str) {
+    const vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    for ( let char of str.toLowerCase()) {
+        if (vowelCheck.includes(char)) {
+            count++;
+        }   
+    }
+    return count;
+}
+
+console.log(vowels('Coding Money')); 
