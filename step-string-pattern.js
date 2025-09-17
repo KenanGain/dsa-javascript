e22932e60ea9b9664e854b87a692c34e00fe3338
Kenan Bharatbhai Gain <123521687+KenanGain@users.noreply.github.com>
2025-09-17 16:52:31 -0400

    step string

---
+++ step-string-pattern.js
@@ -0,0 +1,16 @@
+function steps(n) {
+    for ( let row = 1; row <= n; row++) { 
+        let line = '';
+        for ( let col = 1; col <= n; col++) {
+            if ( col <= row ) {
+                line += '#';
+            } else {        
+            line += ''
+            }
+        }
+        console.log(line);
+    }
+}
+
+
+console.log(steps(6)); // Output:
