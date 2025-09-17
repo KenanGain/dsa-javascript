c576fc6fc438ddbe2d38bb9c32197c5134753100
Kenan Bharatbhai Gain <123521687+KenanGain@users.noreply.github.com>
2025-09-17 15:45:01 -0400

    anagram2\

--- anagram.js
+++ anagram.js
@@ -3,5 +3,27 @@ function anagram(str1, str2) {
     return normalize(str1) === normalize(str2);
 }
 
-// console.log(anagram('rail safety', 'fairy tales')); // Output: true
-console.log(anagram('hello', 'world')); // Output: false
+console.log(isAnagram('rail safety', 'fairy tales')); // Output: true
+// console.log(isAnagram('hello', 'world')); // Output: false
+
+
+function isAnagram(str1, str2) {
+    const norm = str => str.toLowerCase().replace(/[\W]/g, '');
+
+    a = norm(str1); b = norm(str2);
+
+    if (a.length !== b.length) {
+        return false;
+    }
+
+    const count = {};
+
+    for ( let char of a) count[char] = (count[char] || 0) + 1;
+    for ( let char of b) {
+        if (!count[char]) {
+            return false;
+        }   
+        count[char]--;
+    }
+    return true;
+}
